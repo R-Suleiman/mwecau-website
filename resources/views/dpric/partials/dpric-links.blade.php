3f45802fe29fb457c9083ea59cf298e0d364199c
R Suleiman <r.suleiman2901@gmail.com>
2024-10-28 08:27:18 -0700

    Project UI continuation and research layout created

---
+++ resources/views/dpric/partials/dpric-links.blade.php
@@ -0,0 +1,13 @@
+<!-- for title img -->
+<x-application-logo />
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css" />
+
+{{-- <link rel="stylesheet" href="{{ asset('../css/animate.css/animate.css') }}"> --}}
+<link rel="stylesheet" href="{{ asset('../fontawesome/css/all.min.css') }}">
+<link rel="stylesheet" href="{{ asset('../css/dpric/dpric.css') }}">
+{{-- <link rel="stylesheet" href="{{ asset('css/aos/dist/aos.css') }}"> --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
